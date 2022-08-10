564e1156c3786ddaa8bb15d61f461aac66364f3a
DorianBadel <101052235+DorianBadel@users.noreply.github.com>
2022-08-10 16:14:54 +0100

    Added splash screen

--- app/src/main/java/com/example/shows_your_name/MainActivity.kt
+++ app/src/main/java/com/example/shows_your_name/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.shows_your_name
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import com.example.shows_your_name.database.ShowsViewModelFactory
+import androidx.appcompat.app.AppCompatActivity
 import com.example.shows_your_name.databinding.ActivityMainBinding
-import com.example.shows_your_name.viewModels.ShowsViewModel
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
